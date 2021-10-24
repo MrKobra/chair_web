9613742410959adfccb83c6c3affd15b2a5b9fc7
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-24 21:50:28 +0300

    Add catalog

--- html/js/script.js
+++ html/js/script.js
@@ -16,4 +16,106 @@ $(document).ready(function(){
     $('.home-slider').slick({
         infinite: false
     });
+    // Стилизация
+    $('#catalog-sort').styler();
+    $('#items-count').styler();
+    $('.catalog-sidebar-slider input').styler();
+    // Ввод только цифр
+    $('.catalog-sidebar-slider input').keydown(function (event){
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+            return;
+        } else {
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault();
+            }
+        }
+    })
+    // Ползунок
+    $(function(){
+        if($('#price-slider').length != 0) {
+            var min = $('#price-slider').data('min');
+            var max = $('#price-slider').data('max');
+            $('#price-slider').slider({
+                animate: "slow",
+                range: true,
+                min: min,
+                max: max,
+                values: [$('input[name=min_price]').val(), $('input[name=max_price]').val()],
+                slide: function(event, ui) {
+                    $('input[name=min_price]').val(ui.values[0]);
+                    $('input[name=max_price]').val(ui.values[1]);
+                }
+            })
+        }
+        if($('#weight-slider').length != 0) {
+            var min = $('#weight-slider').data('min');
+            var max = $('#weight-slider').data('max');
+            $('#weight-slider').slider({
+                animate: "slow",
+                range: true,
+                min: min,
+                max: max,
+                values: [$('input[name=min_weight]').val(), $('input[name=max_weight]').val()],
+                slide: function(event, ui) {
+                    $('input[name=min_weight]').val(ui.values[0]);
+                    $('input[name=max_weight]').val(ui.values[1]);
+                }
+            })
+        }
+    })
+    // Checkbox
+    $('.catalog-sidebar-row input[type=checkbox]').each(function(){
+        if($(this).prop('checked')) {
+            $(this).parent().addClass('checked')
+        } else {
+            $(this).parent().removeClass('checked')
+        }
+    })
+    $('.catalog-sidebar-row input[type=checkbox]').on('change', function(){
+        if($(this).prop('checked')) {
+            $(this).parent().addClass('checked')
+        } else {
+            $(this).parent().removeClass('checked')
+        }
+    })
+    // Скрыть пункты
+    function hideSidebarCheckbox() {
+        $('.catalog-sidebar-checkbox').each(function(){
+            var count = $(this).find('.catalog-sidebar-row').length;
+            if(count > 7) {
+                var i = 0;
+                $(this).find('.catalog-sidebar-row').each(function(){
+                    i++;
+                    if(i > 7) {
+                        $(this).hide();
+                    }
+                    if(i == count && $(this).parent().find('.show-full-filter').length == 0) {
+                        $(this).after('<a href="#" class="show-full-filter show">Показать еще ('+(count - 7)+')</a>');
+                    }
+                })
+            }
+        })
+    }
+    hideSidebarCheckbox();
+    // Показать пункты
+    $('.show-full-filter').on('click', function(e){
+        e.preventDefault();
+        if($(this).hasClass('show')) {
+            $(this).removeClass('show');
+            $(this).addClass('hide');
+            $(this).parent().find('.catalog-sidebar-row').show();
+            $(this).text('Скрыть');
+        } else {
+            $(this).removeClass('hide');
+            $(this).addClass('show');
+            hideSidebarCheckbox();
+        }
+    })
+    // Показать блоки
+    $('.catalog-sidebar-checkbox .catalog-sidebar-header').on('click', function(){
+        $(this).parent().toggleClass('active');
+        $(this).parent().find('.catalog-sidebar-body').slideToggle(500);
+    })
 })
