bdc13b54550cde4a4534e79fb808593bce269bb3
Mr-Kobra <red.kras.2013@gmail.com>
2021-10-29 00:25:22 +0300

    Add footer block

--- wordpress/wp-content/themes/chair/footer.php
+++ wordpress/wp-content/themes/chair/footer.php
@@ -11,23 +11,86 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'chair' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'chair' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'chair' ), 'chair', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<footer class="main-footer">
+    <div class="container">
+        <div class="main-footer-top">
+            <?php $footer_logo = get_field('footer_logo', 'options');
+            if($footer_logo): ?>
+            <div class="logo">
+                <a href="<?php echo home_url() ?>">
+                    <img src="<?php echo $footer_logo['url'] ?>" alt="<?php echo $footer_logo['alt'] ?>">
+                </a>
+            </div>
+            <?php endif; ?>
+            <div class="main-footer-menu">
+                <div class="main-footer-heading">
+                    <h3>Покупателям</h3>
+                </div>
+                <?php
+                wp_nav_menu( [
+                    'theme_location'  => 'client_menu',
+                    'container'       => '',
+                    'items_wrap'      => '<ul>%3$s</ul>',
+                    'depth'           => 0,
+                    'walker'          => '',
+                ] );
+                ?>
+            </div>
+            <div class="main-footer-menu">
+                <div class="main-footer-heading">
+                    <h3>Помощь</h3>
+                </div>
+                <?php
+                wp_nav_menu( [
+                    'theme_location'  => 'support_menu',
+                    'container'       => '',
+                    'items_wrap'      => '<ul>%3$s</ul>',
+                    'depth'           => 0,
+                    'walker'          => '',
+                ] );
+                ?>
+            </div>
+            <?php if(get_field('footer_contact', 'options')): ?>
+            <div class="main-footer-contact">
+                <div class="main-footer-heading">
+                    <h3>Контакты</h3>
+                </div>
+                <?php the_field('footer_contact', 'options'); ?>
+            </div>
+            <?php endif; ?>
+        </div>
+        <div class="main-footer-bottom">
+            <?php if(get_field('copy', 'options')): ?>
+            <div class="copy">
+                <?php the_field('copy', 'options'); ?>
+            </div>
+            <?php endif; ?>
+            <ul class="payment">
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/img/mir.png" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/img/visa.png" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/img/mastercard.png" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri() ?>/assets/img/maestro.png" alt=""></li>
+            </ul>
+            <ul class="social">
+                <?php if(get_field('telegram_link', 'options')): ?>
+                <li><a href="<?php the_field('telegram_link', 'options'); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/telegram.png" alt=""></a></li>
+                <?php endif; ?>
+                <?php if(get_field('vk_link', 'options')): ?>
+                <li><a href="<?php the_field('vk_link', 'options'); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/vk.png" alt=""></a></li>
+                <?php endif; ?>
+                <?php if(get_field('instagram_link', 'options')): ?>
+                <li><a href="<?php the_field('instagram_link', 'options'); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/instagram.png" alt=""></a></li>
+                <?php endif; ?>
+                <?php if(get_field('youtube_link', 'options')): ?>
+                <li><a href="<?php the_field('youtube_link', 'options'); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/youtube.png" alt=""></a></li>
+                <?php endif; ?>
+                <?php if(get_field('fb_link', 'options')): ?>
+                <li><a href="<?php the_field('fb_link', 'options'); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/facebook.png" alt=""></a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
